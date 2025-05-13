21f4a4a8111280bb8d7d11fe4833e3284cc689dd
Pacoco1 <135071948+Pacoco1@users.noreply.github.com>
2025-05-13 11:54:56 -0600

    Add files via upload

---
+++ PAUSE-SCREEN-DATA.js
@@ -0,0 +1,26 @@
+const pauseScreenData = [
+  {
+    questionIndex: 7,
+    image: "images/gratitude.jpg",
+    quote: "Gratitude unlocks the fullness of life. It turns what we have into enough, and more...",
+    author: "Melody Beattie"
+  },
+  {
+    questionIndex: 14,
+    image: "images/authenticity.jpg",
+    quote: "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
+    author: "Ralph Waldo Emerson"
+  },
+  {
+    questionIndex: 21,
+    image: "images/right-path.jpg",
+    quote: "The right path and easy path are usually two different things... Always choose the right path over the easy.",
+    author: "Deatri King-Bey"
+  },
+  {
+    questionIndex: 25,
+    image: "images/life-love.jpg",
+    quote: "Love the life you live. Live the life you love.",
+    author: "Bob Marley"
+  }
+];
